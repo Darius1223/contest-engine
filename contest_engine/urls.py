4533b6a45bbbdedc73304ffdf965d153dfa6b14f
darius1223 <dar.290199@mail.ru>
2021-03-22 11:04:15 +0700

    init

--- contest_engine/urls.py
+++ contest_engine/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contest/', include('contest.urls'))
+    path('contest/', include('contest.urls'), name='contest'),
+    path('', (lambda request: redirect('/contest')))
 ]
